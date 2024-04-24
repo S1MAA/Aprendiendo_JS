f54eeb254a0524e525cacb39bfe9211cace66e90
S1MAA <sofia.munoz.a@gmail.com>
2024-04-24 16:13:31 -0400

    curso parte uno 1:33:24

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'adicion'); // adición
+// console.log(a - b, 'resta'); // resta 
+// console.log(a * b, 'multiplicacion'); // multiplicación
+// console.log(a / b, 'division'); // división 
+// console.log(a % b, 'modulo'); //módulo 7%5 = 2 cabe una vez y sobra 2
+// console.log(a ** b, 'potencia'); // potencia 
+
+//incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
